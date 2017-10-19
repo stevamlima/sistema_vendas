748aee3298f1f9cb9de653e8b7deb85a94cffeae
Fernando Henrique <fernando.guerra@corujasdev.com.br>
2017-10-19 20:47:48 -0200

    estrutura projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sistema_vendas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
