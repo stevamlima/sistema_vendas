545f36b9e2ce3ae530377f0837501793f5b7bc06
Fernando Henrique <fernando.guerra@corujasdev.com.br>
2017-10-19 22:04:51 -0200

    Validação documentos

--- Program.cs
+++ Program.cs
@@ -22,22 +22,99 @@ namespace sistema_vendas
 
                 switch(opcao){
                     case "1":
-                        Console.WriteLine("Cadastrar Cliente");
+                        CadastrarCliente();
                         break;
                     case "2":
-                        Console.WriteLine("Cadastrar Produto");
+                        CadastrarProduto();
                         break;
                     case "3":
-                        Console.WriteLine("Realizar Venda");
+                        RealizarVenda();
                         break;
                     case "4":
-                        Console.WriteLine("Extrato Cliente");
+                        ExtratoCliente();
                         break;
                 }
 
            }while(opcao != "9");
+        }
 
+        //Cadastra um novo cliente
+        static void CadastrarCliente(){
+            string cpf = "";
+
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu email");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Digite a opção: ");
+            Console.WriteLine("Opção 1 : Pessoa Física ");
+            Console.WriteLine("Opção 2 : Pessoa Jurídica ");
+            string opcao = Console.ReadLine();
+            
+            switch(opcao){
+                case "1":
+                {
+                    bool cpfvalido = false;
+                   
+                    do{
+                        Console.WriteLine("Digite seu cpf:");
+                        cpf = Console.ReadLine();
+
+                        cpfvalido = ValidaCpf(cpf);
+
+                        if(cpfvalido == false)
+                        {
+                            Console.WriteLine("Cpf Inválido");
+                        }
+                    }while(cpfvalido != false);
+
+                    Console.WriteLine("Gravar dados clientes arquivo texto");
+                    break;
+                }
+                case "2":
+                {
+                    break;
+                }
+
+            }
+        }
+
+        //Cadastrar Novo Produto
+        static void CadastrarProduto(){
 
         }
+
+        //Realizar Venda
+        static void RealizarVenda(){
+
+        }
+
+        //Extrato do cliente
+        static void ExtratoCliente(){
+
+        }
+
+        //Metodo para validar o cpf passado 
+        static bool ValidaCpf(string cpf){
+            bool cpfvalido = false;
+
+            if(cpf == "29892294841")
+                cpfvalido = true;
+
+  
+            return cpfvalido;
+        }
+
+        static bool ValidaCnpj(string cnpj){
+            bool cnpjvalido = false;
+
+            if(cnpj == "29892294841")
+                cnpjvalido = true;
+
+            return cnpjvalido;
+        }
+
     }
 }
